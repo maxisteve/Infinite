cab517b2cf1f8b505bd136585b73d1952ff40978
enkaysmartsolutions <enkaysmartsolutions@gmail.com>
2018-01-18 21:08:33 +0530

    Dashboard

--- EnkaySolution/Scripts/AngularController/Inquiry/Dashboard.js
+++ EnkaySolution/Scripts/AngularController/Inquiry/Dashboard.js
@@ -5,8 +5,12 @@
     { name: "Nephi", age: 29 },
     { name: "Enos", age: 34 }];
     $scope.gridOptions = {
+        enableRowSelection: true,
+        enableSelectAll: true,
+        enableFiltering: true,
+        rowHeight: 30,
         data: "myData",
-        columnDefs: [{ field: 'name', displayName: 'First Name', width: 90},
+        columnDefs: [{ field: 'name', displayName: 'First Name',height:3},
             { field: 'age', cellClass: 'ageCell', headerClass: 'ageHeader' }]
     };
 }]);
