059525a5fcfd44759e0d89f6cab842fd31a23c2e
Maxisteve0201 <Maxisteve0201@outlook.com>
2018-01-20 09:00:54 +0530

    inquiry page changes

---
+++ EnkaySolution/Scripts/AngularController/Inquiry/add.js
@@ -0,0 +1,22 @@
+﻿app.controller('InquiryAddCtrl', ['$scope', function ($scope) {
+
+    $scope.goadd = function () {
+
+    }
+
+
+    $scope.myData = [{ name: "Moroni", age: 50 },
+    { name: "Tiancum", age: 43 },
+    { name: "Jacob", age: 27 },
+    { name: "Nephi", age: 29 },
+    { name: "Enos", age: 34 }];
+    $scope.gridOptions = {
+        enableRowSelection: true,
+        enableSelectAll: true,
+        enableFiltering: true,
+        rowHeight: 30,
+        data: "myData",
+        columnDefs: [{ field: 'name', displayName: 'First Name', height: 3 },
+        { field: 'age', cellClass: 'ageCell', headerClass: 'ageHeader' }]
+    };
+}]);
