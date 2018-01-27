fab3748d2533889dbec5322521fe231a1c45e248
Maxisteve0201 <Maxisteve0201@outlook.com>
2018-01-27 08:24:58 +0530

    login ui changes

---
+++ EnkaySolution/Scripts/AngularController/Admin/Login.js
@@ -0,0 +1,22 @@
+﻿app.controller('LoginCtrl', ['$scope','$location', function ($scope, $location, $rootScope) {
+
+    $scope.loginData = {
+        userName: null,
+        passWord: null
+    }
+
+    $scope.login = function ()
+    {
+        alert($scope.loginData.userName);
+
+    }
+    $scope.attemptLogin = function () {
+        if ($scope.loginData.userName == $scope.loginData.passWord) { // test
+            alert('test Pass');
+            $location.path("/Inquiry/add");
+        } else {
+            alert('test Fail');
+            $scope.loginError = "Invalid user/pass.";
+        }
+    }
+}]);
---
+++ EnkaySolution/Scripts/Module/adminApp.js
@@ -0,0 +1,47 @@
+﻿"use strict"
+
+var app = angular.module("adminApp", ['ui.router', 'ui.grid', 'ui.grid.selection', 'angucomplete-alt']);
+ 
+
+app.config(function ($stateProvider) {
+
+    var defaultroute = {
+        name: 'default',
+        url: '/Inquiry/add',
+        templateUrl: '../../Views/Inquiry/Dashboard.html'
+    }
+
+    var Inquiry = {
+        name: 'Inquiry',
+        url: '  ',
+        templateUrl: '../../Views/Inquiry/Dashboard.html'
+
+
+    }
+
+    var Quote = {
+        name: 'Quote',
+        url: '/Quote/add',
+        templateUrl: '../../Views/Quote/add.html'
+    }
+
+    var Service = {
+        name: 'Service',
+        url: '/Service/add',
+        templateUrl: '../../Views/Service/add.html'
+    }
+
+    var aboutState = {
+        name: 'about',
+        url: '/about',
+        template: '<h3>Its the UI-Router hello world app!</h3>'
+    }
+
+
+    $stateProvider.state(defaultroute);
+    $stateProvider.state(Inquiry);
+    $stateProvider.state(Quote);
+    $stateProvider.state(Service);
+
+
+});
